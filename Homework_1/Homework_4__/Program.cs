6407d66e4179d9ac4928a9d3a8fdd77f9ac6ad2b
Svetlana <sveta_gyk@mail.ru>
2022-06-27 01:07:49 +0300

    Homework 4

---
+++ Homework_1/Homework_4__/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+/* Console.WriteLine("Введите целое число А");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число B");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Число А возведенной в натуральную степень числа В ");
+Console.Write(Math.Pow(num, num2) + " "); */
+
+
+/* Задача 27: Напишите программу, которая принимает на
+вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+/* Console.WriteLine("Введите целое число");
+int a = int.Parse(Console.ReadLine());
+int s = 0;
+while (a > 0)
+{
+    s = s + a % 10;
+    a = a / 10;
+}
+Console.WriteLine("Сумма цифр в числе");
+Console.WriteLine(s);
+ */
+
+/* Задача 29: Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33-> [6, 1, 33] */
+
+/* int [] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 9);
+  
+}
+void PrintArray (int [] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+}
+PrintArray(array); */
---
+++ Homework_1/Homework_5/Program.cs
@@ -0,0 +1,109 @@
+﻿/* Задача 34: Задайте массив заполненный случайными
+положительными трёхзначными числами. Напишите
+программу, которая покажет количество чётных чисел в
+массиве.
+[345, 897, 568, 234] -> 2 */
+
+/* void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = new int[9];
+for (int i = 0; i < arr.Length; i++)
+
+{
+    arr[i] = new Random().Next(100, 1000);
+}
+
+PrintArray(arr);
+
+int count = 0;
+int CountEven(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+CountEven(arr);
+Console.WriteLine($"Количество четных чисел в массиве: {count}"); */
+
+/* Задача 36: Задайте одномерный массив, заполненный
+случайными числами. Найдите сумму элементов, стоящих
+на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+/* void PrintArray(int [] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = new int[4];
+for (int i = 0; i < arr.Length; i++)
+
+{
+    arr[i] = new Random().Next(-1000, 1000);
+}
+
+PrintArray(arr);
+
+int sum = 0;
+int SumUnEvenPos(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i+=2)
+    {
+        sum += collection[i];
+    }
+    return sum;
+}
+
+SumUnEvenPos(arr);
+Console.WriteLine($"Сумма элементов массива, стоящих на нечётных позициях.: {sum}"); */
+
+
+/* Задача 38: Задайте массив вещественных чисел. Найдите
+разницу между максимальным и минимальным
+элементов массива.
+[3 7 22 2 78] -> 76 */
+
+void PrintArray(double [] col)
+{
+    for (double i = 0; i < col.Length; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+double[] arr = new double[5];
+for (double i = 0; i < arr.Length; i++)
+
+{
+    arr[i] = Math.Round(new Random().NextDouble());
+}
+
+PrintArray(arr);
+
+double FindExtremDiff(double[] collection);
+{
+    for (double i = 0; i < collection.Length; i++)
+    {
+        FindExtremDiff += collection[i];
+    }
+    return FindExtremDiff;
+}
+
+FindExtremDiff(arr);
+Console.WriteLine($"Сумма элементов массива, стоящих на нечётных позициях.: {FindExtremDiff}");
